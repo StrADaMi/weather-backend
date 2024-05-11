65cb694a8d88e47df0ad8b6b4d31a20536643bba
damianstrag <damian_jan@onet.eu>
2024-05-11 14:02:42 +0200

    Project

--- src/main/java/com/weather/backend/api/controller/WeatherController.java
+++ src/main/java/com/weather/backend/api/controller/WeatherController.java
@@ -5,11 +5,16 @@ import com.weather.backend.api.mapper.WeatherDtoMapper;
 import com.weather.backend.api.validator.ValidCoordinate;
 import com.weather.backend.domain.model.DayWeatherWithEnergyProduction;
 import com.weather.backend.domain.service.WeatherService;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import lombok.AllArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -23,15 +28,16 @@ public class WeatherController {
 
     @GetMapping("/api/v1/weather/daily")
     public ResponseEntity<WeatherCollectionDto> content(
-        @ValidCoordinate(min = -90, max = 90)
-        @RequestParam(value = "latitude", required = false) String latitude,
+            @Min(value = 18, message = "Age should not be less than 18")
+            @Max(value = 150, message = "Age should not be greater than 150")
+            @RequestParam(value = "latitude", required = false) String latitude,
 
-        @ValidCoordinate
-        @RequestParam(value = "longitude", required = false) String longitude) {
+            @ValidCoordinate
+            @RequestParam(value = "longitude", required = false) String longitude) {
 
         List<DayWeatherWithEnergyProduction> weathers = weatherService.getWeather(latitude, longitude);
         WeatherCollectionDto weather = weatherDtoMapper.mapToDto(weathers);
         return ResponseEntity.ok()
-            .body(weather);
+                .body(weather);
     }
 }
